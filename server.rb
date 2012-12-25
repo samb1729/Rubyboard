92525fd4e14524f7cf9c79f0870d5d02fbf3c532
Sam Broughton <sam@26th-zerk.co.uk>
2012-12-25 20:06:42 +0000

    Save name after each post

--- server.rb
+++ server.rb
@@ -6,6 +6,8 @@ require 'bb-ruby'
 
 set :port, 80
 
+enable :sessions
+
 helpers do
   def h text
     Rack::Utils.escape_html text
@@ -14,6 +16,7 @@ end
 
 before '/' do
   @@posts ||= []
+  session ||= []
 end
 
 get '/' do
@@ -22,6 +25,7 @@ end
 
 post '/post' do
   name, content = params[:name], params[:post]
+  session['name'] = name
   unless name.length < 3 or content.length < 10
     @@posts << Post.new(name, content.bbcode_to_html({}, true, :disable, :image), Time.now)
   end
