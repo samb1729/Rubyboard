405a4b84f91610af624c639553c1777ee87f7255
Sam Broughton <sam@26th-zerk.co.uk>
2012-12-25 21:08:11 +0000

    Added database and post wait limit
    Post time limit seems pretty hacky, could use a fix

--- post.rb
+++ post.rb
@@ -1,20 +1,15 @@
+require 'data_mapper'
+
 def h text
   Rack::Utils.escape_html text
 end
 
 class Post
-  attr_accessor :name, :content, :time
+  include DataMapper::Resource
 
-  def initialize name, content, time
-    @name, @content, @time = name, content, time
-  end
-
-  def name
-    return "<strong>" << @name << "</strong>" unless @name =~ /#/
-
-    name_with_trip = @name.split '#', 2
-    trip = Digest::MD5.hexdigest(name_with_trip[1] << "salt to annoy duk")[0..8]
-
-    return "<strong>" << h(name_with_trip[0]) << "</strong>" << " !" << trip
-  end
+  property :id, Serial
+  property :name, String
+  property :tripcode, String
+  property :content, Text
+  property :created_at, DateTime
 end
--- server.rb
+++ server.rb
@@ -3,31 +3,44 @@ $: << File.dirname(__FILE__)
 require 'sinatra'
 require 'post'
 require 'bb-ruby'
+require 'data_mapper'
+require 'dm-migrations'
 
 set :port, 80
-
 enable :sessions
+
+DataMapper.setup(:default, 'sqlite:///'<< File.expand_path('../board.db', __FILE__))
+DataMapper.finalize
+DataMapper.auto_migrate! unless Post.all.count > 0
 
 helpers do
   def h text
     Rack::Utils.escape_html text
-  end  
+  end
+  def trip tripcode
+    trip = Digest::MD5.hexdigest(tripcode << "salt to annoy duk")[0..8]
+  end
 end
 
-before '/' do
-  @@posts ||= []
+before do
   session ||= []
 end
 
 get '/' do
+  @posts = Post.all
   erb :main_page, :layout => :layout
 end
 
 post '/post' do
   name, content = params[:name], params[:post]
   session['name'] = name
-  unless name.length < 3 or content.length < 10
-    @@posts << Post.new(name, content.bbcode_to_html({}, true, :disable, :image), Time.now)
+  session['lastpost'] ||= Time.now.strftime("%s").to_i - 10
+
+  name, tripcode = name.split('#', 2)
+
+  unless name.length < 3 or content.length < 10 or Time.now.strftime("%s").to_i - session['lastpost'].to_i < 10
+    post = Post.create :name => name, :tripcode => tripcode, :content => content, :created_at => Time.now
+    post.save
   end
   redirect '/'
 end
