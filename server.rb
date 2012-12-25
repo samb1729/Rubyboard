166308c3a0f0072c62608004fcd97dde808a721e
Sam Broughton <sam@26th-zerk.co.uk>
2012-12-25 21:53:59 +0000

    Added quoting

--- server.rb
+++ server.rb
@@ -17,6 +17,7 @@ helpers do
   def h text
     Rack::Utils.escape_html text
   end
+
   def trip tripcode
     return "" unless tripcode
     trip = "!" << Digest::MD5.hexdigest(tripcode << "salt to annoy duk")[0..8]
@@ -29,6 +30,15 @@ end
 
 get '/' do
   @posts = Post.all
+  @bbcodes = {
+    'Quote' => [
+      /&gt;&gt;([0-9]*)/,
+      '<a href="#\1">>>\1</a>',
+      'Meme arrows',
+      '>>123',
+      :quote
+    ]
+  }
   erb :main_page, :layout => :layout
 end
 
